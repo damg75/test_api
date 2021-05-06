d7a442b039c5a47cd850ec67c893fde247e677f5
Daniel Moran <damg75cd@gmail.com>
2021-05-06 10:46:21 -0400

    all post endpoints

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get
+Route::get('/posts', function(){ //namespace api
+    return Post::all();
+});
